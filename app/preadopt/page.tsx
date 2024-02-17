1340e192083e2fbca7253d3be2a4d580cb621fed
Kumar Sahani <143087720+Kumar-Sahani200@users.noreply.github.com>
2024-02-17 08:51:09 +0530

    UI changes

--- app/preadopt/page.tsx
+++ app/preadopt/page.tsx
@@ -66,7 +66,7 @@ export default function ProductCard() {
                   href={"https://aashrayforanimals.org.in/"}
                   className="bg-[#6d6d6d] hover:bg-[#11101d] text-white text-sm  py-2 px-3 rounded-full"
                 >
-                  Donate to the Shelter
+                  Donate - Shelter
                 </Link>
               </CardFooter>
             </CardContent>

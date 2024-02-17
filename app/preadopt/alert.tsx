34bceada7a94f7536f29dfcd8f6940684743a763
Kumar Sahani <143087720+Kumar-Sahani200@users.noreply.github.com>
2024-02-17 08:22:00 +0530

    Added PreAdopt Functionality

---
+++ app/preadopt/alert.tsx
@@ -0,0 +1,41 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function AlertRepo() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="bg-[#6d6d6d] hover:bg-[#11101d] text-white text-sm  py-2 px-3 rounded-full">
+          Contact Shelter
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Contact the shelter now</AlertDialogTitle>
+          <AlertDialogDescription>
+            Please be petient and clear in your message, calls are accepted
+            between Monday to Friday
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+          <AlertDialogAction>
+            <Link href={"tel:+919609600920"}>Call</Link>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
